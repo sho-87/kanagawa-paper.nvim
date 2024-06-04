832c50f61bef540f71cb76dff88127ed6fabc0ab
Simon Ho <simonho.ubc@gmail.com>
2024-06-03 18:21:40 -0700

    fix: make tabs and buffers easier to identify

--- lua/kanagawa-paper/highlights/plugins.lua
+++ lua/kanagawa-paper/highlights/plugins.lua
@@ -241,8 +241,10 @@ function M.setup(colors, opts)
 		BufferlineModified = { fg = theme.vcs.changed },
 		BufferlineModifiedSelected = { fg = theme.vcs.changed },
 		BufferlineTabSelected = { bg = theme.ui.bg_p1, fg = palette.dragonWhite },
+		BufferlineTabSeparatorSelected = { link = "BufferlineTabSelected" },
 		BufferlineTabClose = { fg = palette.dragonRed },
 		BufferlineCloseButtonSelected = { fg = palette.dragonRed },
+		BufferlineCloseButton = { fg = "none" },
 
 		-- Nvim-Navic
 		NavicIconsFile = { link = "Directory" },

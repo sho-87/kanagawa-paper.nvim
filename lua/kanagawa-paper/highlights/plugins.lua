cb5b5978654651d37d22066c9b59d2a38489bc36
Simon Ho <simonho.ubc@gmail.com>
2024-09-08 22:23:30 -0700

    fix(markdown): fix header colors

--- lua/kanagawa-paper/highlights/plugins.lua
+++ lua/kanagawa-paper/highlights/plugins.lua
@@ -247,19 +247,43 @@ function M.setup(colors, opts)
 		Headline6 = { bg = tostring(color(theme.rainbow.rainbow6):brighten(-0.4)), fg = theme.ui.fg_dark, bold = true },
 
 		-- markdown.nvim
-		RenderMarkdownH1Bg = { bg = theme.rainbow.rainbow1, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH2Bg = { bg = theme.rainbow.rainbow2, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH3Bg = { bg = theme.rainbow.rainbow3, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH4Bg = { bg = theme.rainbow.rainbow4, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH5Bg = { bg = theme.rainbow.rainbow5, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH6Bg = { bg = theme.rainbow.rainbow6, fg = theme.ui.fg_dark, bold = true },
+		RenderMarkdownH1 = { fg = theme.rainbow.rainbow1 },
+		RenderMarkdownH2 = { fg = theme.rainbow.rainbow2 },
+		RenderMarkdownH3 = { fg = theme.rainbow.rainbow3 },
+		RenderMarkdownH4 = { fg = theme.rainbow.rainbow4 },
+		RenderMarkdownH5 = { fg = theme.rainbow.rainbow5 },
+		RenderMarkdownH6 = { fg = theme.rainbow.rainbow6 },
 
-		RenderMarkdownH1 = { bg = theme.rainbow.rainbow1, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH2 = { bg = theme.rainbow.rainbow2, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH3 = { bg = theme.rainbow.rainbow3, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH4 = { bg = theme.rainbow.rainbow4, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH5 = { bg = theme.rainbow.rainbow5, fg = theme.ui.fg_dark, bold = true },
-		RenderMarkdownH6 = { bg = theme.rainbow.rainbow6, fg = theme.ui.fg_dark, bold = true },
+		RenderMarkdownH1Bg = {
+			fg = theme.rainbow.rainbow1,
+			bg = tostring(color(theme.rainbow.rainbow1):brighten(-0.6)),
+			bold = true,
+		},
+		RenderMarkdownH2Bg = {
+			fg = theme.rainbow.rainbow2,
+			bg = tostring(color(theme.rainbow.rainbow2):brighten(-0.6)),
+			bold = true,
+		},
+		RenderMarkdownH3Bg = {
+			fg = theme.rainbow.rainbow3,
+			bg = tostring(color(theme.rainbow.rainbow3):brighten(-0.6)),
+			bold = true,
+		},
+		RenderMarkdownH4Bg = {
+			fg = theme.rainbow.rainbow4,
+			bg = tostring(color(theme.rainbow.rainbow4):brighten(-0.6)),
+			bold = true,
+		},
+		RenderMarkdownH5Bg = {
+			fg = theme.rainbow.rainbow5,
+			bg = tostring(color(theme.rainbow.rainbow5):brighten(-0.6)),
+			bold = true,
+		},
+		RenderMarkdownH6Bg = {
+			fg = theme.rainbow.rainbow6,
+			bg = tostring(color(theme.rainbow.rainbow6):brighten(-0.6)),
+			bold = true,
+		},
 
 		-- Raindow delimiters
 		RainbowDelimiterRed = { fg = theme.rainbow.rainbow1 },

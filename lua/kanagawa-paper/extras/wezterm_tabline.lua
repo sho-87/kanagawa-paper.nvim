bdd93df0f430f938cd62f740e283d41e71f25591
Simon Ho <simonho.ubc@gmail.com>
2025-02-21 07:09:17 -0800

    feat(wezterm_tabline): add template for wezterm tabline plugin
    fix(wezterm_tabline): fix section A fg color

---
+++ lua/kanagawa-paper/extras/wezterm_tabline.lua
@@ -0,0 +1,52 @@
+local util = require("kanagawa-paper.lib.util")
+
+local M = {}
+
+--- @param colors ThemeColors
+function M.generate(colors)
+	local wezterm_tabline = util.template(
+		[[
+-----------------------------------------------------------------------------
+--- ${_style_name}
+--- Upstream: ${_upstream_url}
+--- URL: ${_url}
+-----------------------------------------------------------------------------
+
+local M = {}
+
+M.theme_overrides = {
+  normal_mode = {
+    a = { fg = "${ui.bg_p1}", bg = "${modes.normal}" },
+    b = { fg = "${modes.normal}", bg = "${ui.bg_p1}" },
+    c = { fg = "${ui.fg_gray}", bg = "${ui.bg_statusline}" },
+  },
+  copy_mode = {
+    a = { fg = "${ui.bg_p1}", bg = "${modes.insert}" },
+    b = { fg = "${modes.insert}", bg = "${ui.bg_p1}" },
+    c = { fg = "${ui.fg_gray}", bg = "${ui.bg_statusline}" },
+  },
+  search_mode = {
+    a = { fg = "${ui.bg_p1}", bg = "${modes.visual}" },
+    b = { fg = "${modes.visual}", bg = "${ui.bg_p1}" },
+    c = { fg = "${ui.fg_gray}", bg = "${ui.bg_statusline}" },
+  },
+  window_mode = {
+    a = { fg = "${ui.bg_p1}", bg = "${accent.accent1}" },
+    b = { fg = "${accent.accent1}", bg = "${ui.bg_p1}" },
+    c = { fg = "${ui.fg_gray}", bg = "${ui.bg_statusline}" },
+  },
+  resize_mode = {
+    a = { fg = "${ui.bg_p1}", bg = "${accent.accent2}" },
+    b = { fg = "${accent.accent2}", bg = "${ui.bg_p1}" },
+    c = { fg = "${ui.fg_gray}", bg = "${ui.bg_statusline}" },
+  },
+}
+
+return M
+]],
+		colors
+	)
+	return wezterm_tabline
+end
+
+return M

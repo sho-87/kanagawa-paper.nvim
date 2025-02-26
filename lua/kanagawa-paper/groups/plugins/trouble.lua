917dcb84f9d72cfb59ad2c92d1d96af19e4fc292
Simon Ho <simonho.ubc@gmail.com>
2025-02-25 18:20:29 -0800

    fix(telescope): add missing hlgroups

--- lua/kanagawa-paper/groups/plugins/trouble.lua
+++ lua/kanagawa-paper/groups/plugins/trouble.lua
@@ -4,23 +4,9 @@ function M.get(colors, opts)
 	local theme = colors.theme
 
 	return {
-		TelescopeTitle = { fg = theme.ui.fg_dark, bg = theme.accent.accent1, bold = true },
-		TelescopeBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg },
-		TelescopeSelection = { link = "CursorLine" },
-		TelescopeSelectionCaret = { link = "CursorLineNr" },
-		TelescopePromptNormal = { bg = theme.ui.bg_p2 },
-		TelescopePromptBorder = { fg = theme.ui.bg_p2, bg = theme.ui.bg_p2 },
-		TelescopeResultsClass = { link = "Structure" },
-		TelescopeResultsStruct = { link = "Structure" },
-		TelescopeResultsField = { link = "@field" },
-		TelescopeResultsMethod = { link = "Function" },
-		TelescopeResultsVariable = { link = "@variable" },
-		TeleScopeResultsTitle = { fg = theme.ui.fg_dark, bg = theme.accent.accent2, bold = true },
-		TelescopeResultsNormal = { fg = theme.ui.fg, bg = theme.ui.bg_p1 },
-		TelescopeResultsBorder = { fg = theme.ui.bg_p2, bg = theme.ui.bg_p1 },
-		TeleScopePreviewTitle = { fg = theme.ui.fg_dark, bg = theme.accent.accent3, bold = true },
-		TelescopePreviewNormal = { bg = theme.ui.bg },
-		TelescopePreviewBorder = { bg = theme.ui.bg, fg = theme.ui.bg_p2 },
+		TroubleIndent = { fg = theme.ui.indent },
+		TroublePos = { fg = theme.ui.special },
+		TroubleCount = { fg = theme.syn.special2 },
 	}
 end
 

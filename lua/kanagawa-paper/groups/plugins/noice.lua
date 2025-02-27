31bbe0bfb5262f6e76efc9b6414d3c27e52d38a8
Simon Ho <simonho.ubc@gmail.com>
2025-02-26 19:14:09 -0800

    fix(noice): fix command line search text readability

--- lua/kanagawa-paper/groups/plugins/noice.lua
+++ lua/kanagawa-paper/groups/plugins/noice.lua
@@ -4,8 +4,8 @@ function M.get(colors, opts)
 	local theme = colors.theme
 
 	return {
-		NoiceCmdline = { fg = theme.ui.bg_search },
-		NoiceVirtualText = { fg = theme.ui.bg_search },
+		NoiceCmdline = { fg = theme.ui.fg },
+		NoiceVirtualText = { fg = theme.syn.comment },
 		NoicePopupBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.float.bg_border },
 		NoiceLspProgressTitle = { fg = theme.ui.fg_gray },
 	}

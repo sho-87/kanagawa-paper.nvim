e417ae9e4032543bdfb44fb33af864c6fd83f4b0
thesimonho <5199715+thesimonho@users.noreply.github.com>
2025-02-27 05:04:58 +0000

    build(extras): auto build themes for extras

--- extras/fzf/kanagawa-paper-ink.sh
+++ extras/fzf/kanagawa-paper-ink.sh
@@ -9,4 +9,4 @@
 
 --color=bg:-1,bg+:#363646,fg:-1,fg+:#DCD7BA,hl:#938AA9,hl+:#c4746e
 --color=header:#b6927b,info:#658594,pointer:#7AA89F
---color=marker:#7AA89F,prompt:#c4746e,spinner:#8ea4a2
+--color=marker:#7AA89F,prompt:#c4746e,spinner:#8ea49e
